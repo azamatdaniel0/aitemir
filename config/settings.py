54abfaf8ec50812b748995c1ad78cbcd2f1ef1e2
Azamat Daniel <azamatdaniel0@gmail.com>
2024-06-01 10:44:00 +0600

    fixes

--- config/settings.py
+++ config/settings.py
@@ -26,13 +26,8 @@ SECRET_KEY = 'django-insecure-tltd7f_s*thb6^dusym*zrt3_y(jx*zr7bbo78t8pv5o%4um51
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
-CORS_ALLOW_METHODS = "*"
-CORS_ALLOW_HEADERS = "*"
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-CSRF_COOKIE_SECURE = False
+
+
 
 # Application definition
 
@@ -52,15 +47,22 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
-
+ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_METHODS = "*"
+CORS_ALLOW_HEADERS = "*"
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = ['https://passport.brisklyminds.com']
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
